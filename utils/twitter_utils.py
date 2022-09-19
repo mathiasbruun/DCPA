9ae9e32dc8f5a046679575ba01ae96cb27a908bf
Mathias Bruun <mathiasbruun.mb@gmail.com>
2022-09-19 22:44:40 +0200

    1st twitter collection done; 1st full tweet df constructed

--- utils/twitter_utils.py
+++ utils/twitter_utils.py
@@ -2,16 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
+import numpy as np
 import pandas as pd
 from loguru import logger
 from tqdm.notebook import tqdm
+import pickle
 
 import tweepy
-from tweepy import Unauthorized, NotFound, Forbidden
+from tweepy import Unauthorized, NotFound, Forbidden, TweepyException
 from .tweepy_credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, timeout=120, retry_count=10, retry_delay=1)
 
 def scrape_politician_handles(url = 'https://web.archive.org/web/20220328101917/https://filip.sdu.dk/twitter/politikere/'):
     '''
@@ -82,28 +84,46 @@ def collect_single_user_tweets(screen_name, since_id = None):
     
     return(tweets)
 
+def divide_chunks(l, n):
+    
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 def collect_all_user_tweets(twitter_handle_list):
     '''
     TODO...
     '''
     logger.add("logs/twitter_collection_{time}.log")
     
-    all_tweets = []
+    total_chunks = np.ceil(len(twitter_handle_list)/100)
+    logger.info(f'Dividing handles into {total_chunks} chunk(s)...')
     
+    handle_chunks = divide_chunks(twitter_handle_list, 100)
+
     logger.info('Beginning collection...')
     
-    for handle in tqdm(twitter_handle_list):
+    chunk_no = 1
+    
+    for chunk in handle_chunks:
         
-        try:
-            tweets = collect_single_user_tweets(handle)
-            all_tweets.extend([tweet for tweet in tweets])
-            
-        except (Unauthorized, Forbidden, NotFound) as error:
-            logger.debug(f'{error}: Unable to collect tweets for {handle}')
+        chunk_tweets = []
+        logger.info(f'--- CHUNK NO. {chunk_no} ---')
+        
+        for handle in tqdm(chunk):
+
+            try:
+                tweets = collect_single_user_tweets(handle)
+                chunk_tweets.extend([tweet for tweet in tweets])
+
+            except (Unauthorized, NotFound, Forbidden, TweepyException) as ex:
+                logger.error(f'{ex}! Unable to collect tweets for {handle}')
+        
+        with open(f'data/raw/twitter/chunks/tweet_chunk_{chunk_no}.p', 'wb') as p:
+            pickle.dump(chunk_tweets, p)
+        
+        chunk_no += 1
     
     logger.info(f'Collection completed for {len(twitter_handle_list)} twitter handles!')
-    
-    return(all_tweets)
 
 def construct_tweet_df(all_tweets):
     '''
@@ -113,6 +133,8 @@ def construct_tweet_df(all_tweets):
     cols_to_extract = [
         'screen_name',
         'name',
+        'description',
+        'followers_count',
         'id_str',
         'created_at',
         'full_text'
@@ -121,9 +143,11 @@ def construct_tweet_df(all_tweets):
     df_tweets = pd.DataFrame([[
             tweet['user'][cols_to_extract[0]],
             tweet['user'][cols_to_extract[1]],
-            tweet[cols_to_extract[2]],
-            tweet[cols_to_extract[3]],
-            tweet[cols_to_extract[4]]
+            tweet['user'][cols_to_extract[2]],
+            tweet['user'][cols_to_extract[3]],
+            tweet[cols_to_extract[4]],
+            tweet[cols_to_extract[5]],
+            tweet[cols_to_extract[6]]
         ] for tweet in all_tweets], columns = cols_to_extract
     )
 
@@ -167,9 +191,31 @@ def get_new_user_tweets(df_tweets):
             tweets = collect_single_user_tweets(handle, since_id = most_recent_id)
             all_tweets.extend([tweet for tweet in tweets])
             
-        except (Unauthorized, Forbidden, NotFound) as error:
-            logger.debug(f'{error}: Unable to collect tweets for {handle}')
+        except Exception as ex:
+            ex_name = type(ex).__name__
+            logger.error(f'{ex_name}! Unable to collect tweets for {handle}')
     
     logger.info(f'Collection completed for {len(unique_handles)} twitter handles!')
     
     return(all_tweets)
+
+# Custom data loader here
+
+def tweet_chunks2df(chunks_dir = 'data/raw/twitter/chunks/', no_chunks = 8):
+    '''
+    TODO...
+    '''
+    
+    df_list = []
+    
+    for chunk_no in tqdm(range(1, no_chunks+1)):
+        
+        with open(f'{chunks_dir}tweet_chunk_{chunk_no}.p', 'rb') as p:
+            
+            chunk_tweets = pickle.load(p)
+            df_tweets = construct_tweet_df(chunk_tweets)
+            df_list.append(df_tweets)
+            
+    df_all_tweets = pd.concat(df_list)
+    
+    return(df_all_tweets) 
